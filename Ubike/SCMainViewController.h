cd3656ee16fc17ab5701c01688aa02b7bd0954ba
Johnny <j796160836@gmail.com>
2014-05-10 17:07:37 +0800

    map test

---
+++ Ubike/SCMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCMainViewController.h
+//  Ubike
+//
+//  Created by Johnny on 5/10/14.
+//  Copyright (c) 2014 wpsteak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCMainViewController : UIViewController
+
+@end
---
+++ Ubike/SCMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  SCMainViewController.m
+//  Ubike
+//
+//  Created by Johnny on 5/10/14.
+//  Copyright (c) 2014 wpsteak. All rights reserved.
+//
+
+#import "SCMainViewController.h"
+
+@interface SCMainViewController ()
+
+@end
+
+@implementation SCMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
