efc586e8dae406f232ce5482924793b779849b9c
Johnny <j796160836@gmail.com>
2014-05-10 17:43:40 +0800

    印出路線

--- Ubike/SCMainViewController.m
+++ Ubike/SCMainViewController.m
@@ -13,6 +13,7 @@
 @interface SCMainViewController ()
 
 @property (weak, nonatomic) GMSMapView *mapView;
+@property (strong, nonatomic) NSMutableArray *routeArray;
 
 @end
 
@@ -33,10 +34,10 @@
     
     self.navigationController.title=@"Bikeable";
     
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
-    CGRect frame=CGRectMake(0, 0, 300, 300);
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:25.041294
+                                                            longitude:121.557953
+                                                                 zoom:14];
+    CGRect frame=self.view.bounds;
     self.mapView = [GMSMapView mapWithFrame:frame camera:camera];
     self.mapView.myLocationEnabled = YES;
     [self.view addSubview:self.mapView];
@@ -56,43 +57,65 @@
     london.icon = [UIImage imageNamed:@"house"];
     london.map = self.mapView;
     
+    [self loadRoutePoints];
+    
+    
+
+    // Do any additional setup after loading the view from its nib.
+}
+
+-(void) loadRoutePoints
+{
+    self.routeArray= [[NSMutableArray alloc] init];
+    
     PFQuery *query = [PFQuery queryWithClassName:@"BikeRoutes"];
-//    [query whereKey:@"playerName" equalTo:@"Dan Stemkoski"];
+    //    [query whereKey:@"playerName" equalTo:@"Dan Stemkoski"];
     [query findObjectsInBackgroundWithBlock:^(NSArray *routes, NSError *error) {
         if (!error) {
             NSLog(@"%d Routes", routes.count);
             for (PFObject *route in routes) {
+                
                 NSString *routeId=route.objectId;
                 PFQuery *pointQuery = [PFQuery queryWithClassName:@"BikeRoutePoint"];
                 [pointQuery whereKey:@"RoutesId" equalTo:routeId];
                 [pointQuery findObjectsInBackgroundWithBlock:^(NSArray *points, NSError *error) {
+                    NSMutableArray *routePoints=[[NSMutableArray alloc]init];
                     for (PFObject *point in points) {
                         PFGeoPoint *po=point[@"location"];
-                        
-                        NSLog(@"Lat: %f", po.latitude);
-                        NSLog(@"Lng: %f", po.longitude);
-                        NSLog(@"------");
+                        [routePoints addObject:po];
+//                        NSLog(@"Lat: %f", po.latitude);
+//                        NSLog(@"Lng: %f", po.longitude);
+//                        NSLog(@"------");
                     }
-                    
+                    [self.routeArray addObject:routePoints];
+                    [self drawRoutes];
                 }];
-                //test
-                break;
             }
         } else {
             NSLog(@"Error: %@ %@", error, [error userInfo]);
         }
     }];
-    
-    
-    GMSMutablePath *path = [GMSMutablePath path];
-    [path addCoordinate:CLLocationCoordinate2DMake(-33.85, 151.20)];
-    [path addCoordinate:CLLocationCoordinate2DMake(-33.70, 151.40)];
-    [path addCoordinate:CLLocationCoordinate2DMake(-33.73, 151.41)];
-    GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
-    polyline.strokeColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"pin-01.png"]];
-    
-    polyline.map=self.mapView;
-    // Do any additional setup after loading the view from its nib.
+}
+
+-(void) drawRoutes
+{
+    int i=0;
+    for (NSArray* route in self.routeArray) {
+        
+        GMSMutablePath *path = [GMSMutablePath path];
+        for (PFGeoPoint* po in route) {
+            [path addCoordinate:CLLocationCoordinate2DMake(po.latitude, po.longitude)];
+        }
+        GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
+        if (i%2) {
+            polyline.strokeColor=[UIColor colorWithRed:0.492721F green:0.792157F blue:0.596751F alpha:1.0F];
+        }else{
+            polyline.strokeColor=[UIColor colorWithRed:0.620210F green:0.349216F blue:0.792157F alpha:1.0F];
+        }
+        polyline.strokeWidth=3;
+        polyline.map=self.mapView;
+        i++;
+    }
 }
 
 - (void)didReceiveMemoryWarning
